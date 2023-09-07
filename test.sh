16221deab28e8cc076cc3eaeffbc7825a93f56e5
chrgrbr <chrgro@gmx.de>
2023-09-07 17:45:10 +0200

    Updated Sanity Check

--- test.sh
+++ test.sh
@@ -26,7 +26,7 @@ docker run --rm \
 docker run --rm \
         -v nlst_monai-output-$VOLUME_SUFFIX:/output/ \
         -v $SCRIPTPATH/test/:/input/ \
-        insighttoolkit/simpleitk-notebooks:latest python -c "import SimpleITK, numpy; pred = SimpleITK.GetArrayFromImage(SimpleITK.ReadImage('/output/images/displacement-field/thisIsAnArbitraryFilename.mha')); assert pred.shape == (224, 192, 224, 3)"
+        insighttoolkit/simpleitk-notebooks:latest python -c "import SimpleITK, numpy; pred = SimpleITK.GetArrayFromImage(SimpleITK.ReadImage('/output/images/displacement-field/thisIsAnArbitraryFilename.mha')); ref = SimpleITK.GetArrayFromImage(SimpleITK.ReadImage('/input/reference_disp_0101_0101.mha')); assert numpy.allclose(pred, ref, atol=1e-2)"
 
 if [ $? -eq 0 ]; then
     echo "Tests successfully passed..."
